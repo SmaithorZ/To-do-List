27362eb281972b2fa701acbba07d494f4409b7a8
Mateus Araújo <smaithorz@gmail.com>
2023-06-12 07:03:10 -0300

    Added a remove btn

--- main/script.js
+++ main/script.js
@@ -1,5 +1,6 @@
 let input = document.querySelector("#iprompt");
 let addBtn = document.querySelector("#add-btn");
+let removeBtn = document.querySelector("#remove-btn");
 let ul = document.querySelector("ul");
 let li;
 
@@ -8,4 +9,8 @@ addBtn.addEventListener("click", function () {
   li = document.createElement("li");
   ul.append(li);
   li.innerText = input.value;
+});
+
+removeBtn.addEventListener("click", function () {
+  ul.lastChild.remove();
 });
